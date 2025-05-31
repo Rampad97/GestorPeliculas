a3d83c312ae645550508cc51a6b158129df29c86
LeonardoGalicia <galicia97levi@gmail.com>
2025-05-31 11:30:43 -0600

    se agrega system.out

--- src/com/metaphorce/modelo/GestorPelicula.java
+++ src/com/metaphorce/modelo/GestorPelicula.java
@@ -60,6 +60,7 @@ public class GestorPelicula {
     }
 
     public Pelicula buscarPorNombre(String nombre) {
+        System.out.println("Leonardo");
         for (Pelicula pelicula : peliculas) {
             if (nombre.equals(pelicula.getNombre())) {
                 return pelicula;
